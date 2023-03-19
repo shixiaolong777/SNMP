4c1b9b4d0ceaa08adb1f9d896c5b70a5f3ee43d8
shixiaolong777 <74590603+shixiaolong777@users.noreply.github.com>
2023-03-19 19:48:10 +0800

    Add files via upload

---
+++ SnmpTrap.Listener/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using SnmpSharpNet;
+using SnmpTrap.Lib;
+
+namespace SnmpTrap.Listener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SnmpTrapListener stl = new SnmpTrapListener())
+            {                
+                stl.OnReceiveSnmpTrapPacket = 
+                    new SnmpTrapPacketHandler((remoteEP, pkt) =>
+                    {                        
+                        if (pkt.Version == SnmpVersion.Ver1)
+                        {
+                            SnmpV1TrapPacket v1pkt = pkt as SnmpV1TrapPacket;
+
+                            Console.WriteLine("{0:yyyy-MM-dd HH:mm:ss} receive SNMPv1 TRAP from {1}:{2}",
+                                DateTime.Now, remoteEP.Address.ToString(), remoteEP.Port);
+                            
+                            Console.WriteLine("*** community {0} generic id: {1} specific id: {2}",
+                                v1pkt.Community, v1pkt.Pdu.Generic, v1pkt.Pdu.Specific);
+                            
+                            Console.WriteLine("*** PDU Count: {0}", v1pkt.Pdu.VbCount);
+                            
+                            foreach (Vb vb in v1pkt.Pdu.VbList)
+                            {                                
+                                OutputTrapResult(vb);                                
+                            }
+
+                            Console.WriteLine("end of SNMPv1 TRAP");
+                        }
+                        else if (pkt.Version == SnmpVersion.Ver2)
+                        {
+                            SnmpV2Packet v2pkt = pkt as SnmpV2Packet;
+                            
+                            if (v2pkt.Pdu.Type == PduType.V2Trap)
+                            {
+                                Console.WriteLine("{0:yyyy-MM-dd HH:mm:ss} receive SNMPv2 TRAP from {1}:{2}",
+                                    DateTime.Now, remoteEP.Address.ToString(), remoteEP.Port);
+                            }
+                            else if (v2pkt.Pdu.Type == PduType.Inform)
+                            {
+                                Console.WriteLine("{0:yyyy-MM-dd HH:mm:ss} receive SNMPv2 INFORM from {1}:{2}",
+                                    DateTime.Now, remoteEP.Address.ToString(), remoteEP.Port);
+                            }
+
+                            Console.WriteLine("*** community {0} sysUpTime: {1} trapObjectID: {2}",
+                                v2pkt.Community, v2pkt.Pdu.TrapSysUpTime, v2pkt.Pdu.TrapObjectID);
+
+                            Console.WriteLine("*** PDU Count: {0}", v2pkt.Pdu.VbCount);
+
+                            foreach (Vb vb in v2pkt.Pdu.VbList)
+                            {
+                                OutputTrapResult(vb);
+                            }
+
+                            if (v2pkt.Pdu.Type == PduType.V2Trap)
+                            {
+                                Console.WriteLine("end of SNMPv2 TRAP");
+                            }
+                            else if (v2pkt.Pdu.Type == PduType.Inform)
+                            {
+                                Console.WriteLine("end of SNMPv2 INFORM");
+                            }
+                        }
+                    });
+                stl.Start();
+                Console.WriteLine("Listening...");
+                Console.Read();
+            }
+        }
+        private const string MS_ENTERPRISE_OID = "1.3.6.1.4.1.311.1.13.1";
+        private static void OutputTrapResult(Vb vb)
+        {
+            string valueType = SnmpConstants.GetTypeName(vb.Value.Type);
+            string oid = vb.Oid.ToString();
+            if (oid.StartsWith(MS_ENTERPRISE_OID) && valueType.Equals("OctetString", StringComparison.OrdinalIgnoreCase) 
+                && vb.Value.ToString().StartsWith("EVNTTRAPBEGIN"))
+            {
+                string eventlogOid = oid.Substring(MS_ENTERPRISE_OID.Length + 1);
+                string[] o = eventlogOid.Split('.');
+                string eventlog_source = string.Empty;
+                for (int i = 1; i < o.Length; i++)
+                {
+                    int x = Convert.ToInt32(o[i]);
+                    eventlog_source += (char)x;
+                }
+                Console.WriteLine("EventLog Source:{0}", eventlog_source);
+                string s = vb.Value.ToString();
+                EventLogTrapMessage msg = EventLogTrapMessage.DeSerialize(s);
+                Console.WriteLine("ActivityId:{0}", msg.ActivityId);
+                Console.WriteLine("ContainerLog:{0}", msg.ContainerLog);
+                Console.WriteLine("Id:{0}", msg.Id);
+                Console.WriteLine("Level:{0}", msg.Level);
+                Console.WriteLine("LevelDisplayName:{0}", msg.LevelDisplayName);
+                Console.WriteLine("LogName:{0}", msg.LogName);
+                Console.WriteLine("MachineName:{0}", msg.MachineName);
+                Console.WriteLine("ProcessId:{0}", msg.ProcessId);
+                if (msg.Properites != null && msg.Properites.Count > 0)
+                {
+                    Console.WriteLine("Properites:{0}", String.Join(",", msg.Properites.Select(p => p)));
+                }
+                else
+                {
+                    Console.WriteLine("Properites:Null");
+                }
+                Console.WriteLine("ProviderId:{0}", msg.ProviderId);
+                Console.WriteLine("ProviderName:{0}", msg.ProviderName);
+                Console.WriteLine("RecordId:{0}", msg.RecordId);
+                Console.WriteLine("RelatedActivityId:{0}", msg.RelatedActivityId);
+                Console.WriteLine("TimeCreated:{0:yyyy-MM-dd HH:mm:ss}", msg.TimeCreated);
+                Console.WriteLine("Version:{0}", msg.Version);
+            }
+            else
+            {
+                Console.WriteLine("*** {0} {1}: {2}", vb.Oid, valueType, vb.Value);
+            }
+        }
+    }
+}
---
+++ SnmpTrap.Listener/SnmpTrapListener.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using SnmpSharpNet;
+
+namespace SnmpTrap.Listener
+{
+    public delegate void SnmpTrapPacketHandler(IPEndPoint remoteEP, SnmpPacket pkt);
+
+    public class SnmpTrapListener : IDisposable
+    {
+        private UdpClient _listener;
+        private IPEndPoint _ep;
+        public SnmpTrapListener(string ip, int port)
+        {
+            _ep = new IPEndPoint(String.IsNullOrEmpty(ip) 
+                ? IPAddress.Any : IPAddress.Parse(ip), port);
+            _listener = new UdpClient(_ep);
+        }
+
+        public SnmpTrapListener(string ip)
+            : this(ip, 162)
+        {
+
+        }
+
+        public SnmpTrapListener() :
+            this(null, 162)
+        {
+
+        }
+
+        public SnmpTrapPacketHandler OnReceiveSnmpTrapPacket;
+
+        public void Start()
+        {
+            _listener.BeginReceive(new AsyncCallback(ReceiveCallBack), _listener);
+        }
+
+        public void Stop()
+        {
+            _listener.Close();
+        }
+
+        private void ReceiveCallBack(IAsyncResult result)
+        {
+            UdpClient client = result.AsyncState as UdpClient;
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            byte[] bytes = client.EndReceive(result, ref remoteEP);
+            Thread t = new Thread(new ParameterizedThreadStart(DualReceivedData));
+            t.Start(new object[2] { remoteEP, bytes });
+            client.BeginReceive(new AsyncCallback(ReceiveCallBack), client);
+        }
+
+        private void DualReceivedData(object data)
+        {
+            IPEndPoint remoteEP = (IPEndPoint)(data as object[])[0];
+            byte[] buffer = (byte[])(data as object[])[1];
+            int pktVersion = SnmpPacket.GetProtocolVersion(buffer, buffer.Length);            
+            switch (pktVersion)
+            {
+                case (int)SnmpVersion.Ver1:
+                    SnmpV1TrapPacket v1pkt = new SnmpV1TrapPacket();
+                    v1pkt.decode(buffer, buffer.Length);
+                    if (OnReceiveSnmpTrapPacket != null)
+                    {
+                        OnReceiveSnmpTrapPacket(remoteEP, v1pkt);
+                    }
+                    break;
+                case (int)SnmpVersion.Ver2:
+                    SnmpV2Packet v2pkt = new SnmpV2Packet();
+                    v2pkt.decode(buffer, buffer.Length);
+                    if (v2pkt.Pdu.Type == PduType.V2Trap || v2pkt.Pdu.Type == PduType.Inform)
+                    {
+                        if (v2pkt.Pdu.Type == PduType.Inform)
+                        {
+                            DualInformRequest(remoteEP, v2pkt);
+                        }
+                        if (OnReceiveSnmpTrapPacket != null)
+                        {
+                            OnReceiveSnmpTrapPacket(remoteEP, v2pkt);
+                        }
+                    }
+                    break;
+                case (int)SnmpVersion.Ver3:
+                    break;
+            }
+        }
+
+        private void DualInformRequest(IPEndPoint remoteEP, SnmpV2Packet pkt)
+        {
+            // send ACK back to the INFORM sender
+            SnmpV2Packet response = pkt.BuildInformResponse();
+            byte[] buffer = response.encode();
+            UdpClient sender = new UdpClient();
+            sender.Send(buffer, buffer.Length, remoteEP);
+        }
+
+        protected void Disposing(bool disposed)
+        {
+            if (disposed)
+            {
+                Stop();
+            }
+        }
+
+        public void Dispose()
+        {
+            Disposing(true);
+        }
+    }
+}
